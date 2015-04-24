f5461880b10e9900733b57abcc5c99d3f85a3b18
srinivaspr <srinivasapavankumarreddy@gmail.com>
2015-04-24 09:05:33 -0700

    log4j version

---
+++ src/main/java/com/ltrflog4j/LtrfLog4jAppender.java
@@ -0,0 +1,99 @@
+package com.ltrflog4j;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Layout;
+import org.apache.log4j.spi.LoggingEvent;
+
+public class LtrfLog4jAppender extends AppenderSkeleton {
+	
+	
+	public LtrfLog4jAppender()
+	{
+		
+	}
+	
+	private String appName;
+	private String keys;
+	private String additionalInfo;
+	private static String[] keyStrings;
+
+	public String getAppName() {
+		return appName;
+	}
+
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+
+	public String getKeys() {
+		return keys;
+	}
+	
+	public String[] getKeyStrings() {
+		return keyStrings;
+	}
+
+	public static void setKeyStrings(String[] keyString) {
+		keyStrings = keyString;
+	}
+
+	public void setKeys(String keys) {
+		this.keys = keys;
+		if(keys != null)
+			setKeyStrings(keys.split(","));
+	}
+
+	public String getAdditionalInfo() {
+		return additionalInfo;
+	}
+
+	public void setAdditionalInfo(String additionalInfo) {
+		this.additionalInfo = additionalInfo;
+	}
+
+	public void close() {
+		// TODO Auto-generated method stub		
+	}
+
+	public boolean requiresLayout() {
+		return true;
+	}
+
+	@Override
+	protected void append(LoggingEvent event) {
+		if(keyStrings == null || keyStrings.length == 0)
+			return;
+		StringBuilder message = new StringBuilder();
+		if (layout != null)
+			message.append(layout.format(event));		
+		if (layout.ignoresThrowable()) {
+			String[] s = event.getThrowableStrRep();
+			if (s != null) {
+				int len = s.length;
+				for (int i = 0; i < len; i++) {
+					message.append(s[i]);
+					message.append(Layout.LINE_SEP);
+				}
+			}
+		}
+		String key;
+		if((key = trackable(message.toString())) != null )
+		{
+			trackMessage(key,message.toString());
+		}
+	}
+
+	private void trackMessage(String key, String message) {
+		System.out.println(key);
+		System.out.println(message);		
+	}
+
+	private String trackable(String message) {
+		for(String key : keyStrings)
+		{
+			if(message.contains(key));
+				return key;
+		}
+		return null;
+	}
+}
